f1bf17111135d530bbb6ea361c3f0f33e6f8fc45
Dip <24651666+superderped@users.noreply.github.com>
2022-07-07 12:29:46 +1000

    Implemented best of 5 games

--- javascript/game.js
+++ javascript/game.js
@@ -10,30 +10,37 @@ function computerPlay() {
     return(computerSelection);
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, score) {
     let gameState = "";
     if (playerSelection == computerSelection) {
         gameState = " Draw!";
     } else if (playerSelection == "rock"){
         if (computerSelection == "paper") {
             gameState = " You Lose!";
+            score[1] = score[1]+1;
         } else if (computerSelection == "scissors"){
-            gameState = " You Win!"
+            gameState = " You Win!";
+            score[0] = score[0]+1;
         }
     }  else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
             gameState = " You Win!";
+            score[0] = score[0]+1;
         } else if (computerSelection == "scissors"){
-            gameState = " You Lose!"
+            gameState = " You Lose!";
+            score[1] = score[1]+1;
         }
     } else {
         if (computerSelection == "paper") {
             gameState = " You Win!";
+            score[0] = score[0]+1;
         } else if (computerSelection == "rock"){
-            gameState = " You Lose!"
+            gameState = " You Lose!";
+            score[1] = score[1]+1;
         }
     }
     console.log("player close: " + playerSelection + " computer chose: " + computerSelection + gameState);
+    return(score);
 }
 
 function startGame() {
@@ -48,6 +55,10 @@ function startGame() {
 }
 
 
-let computerSelection = computerPlay();
-let playerSelection = startGame();
-playRound(playerSelection, computerSelection);
+let score = [0,0]
+for (let i = 0;i < 5; i++){
+    let computerSelection = computerPlay();
+    let playerSelection = startGame();
+    score = playRound(playerSelection, computerSelection, score);
+    console.log("You: " + score[0] + " Computer: " + score[1]);
+}
